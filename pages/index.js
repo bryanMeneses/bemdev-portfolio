92a23f8503024009954d6c40e0bb8c27c8412c99
Bryan Meneses <bryanmeneses@Bryans-MacBook-Pro.local>
2021-02-09 00:20:45 -0800

    finish index

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
         <div className={`row ${styles.row}`}>
 
-          <div className="col-md-5 col-lg-4">
+          <div className={`col-xs-9 col-sm-9 col-md-5 col-lg-4`}>
             <div className={styles['img-wrap']}>
               <div className={styles.img}>
                 <Image width="640" height="640" layout="responsive" src="/bryan-portrait.jpeg" />
@@ -23,12 +23,12 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="col-md-7 col-lg-7">
-            <h5>Hello, I am</h5>
+          <div className={`col-md-7 col-lg-7 ${styles.text}`}>
+            <h5>Front End Developer</h5>
             <h1>Bryan Meneses</h1>
-            <h3>I love to build experiences on the Web</h3>
+            <h4>I love to build experiences on the Web</h4>
             <p className="my-4">I am focusing on front end technologies. React, Next.js, jQuery, Shopify/Liquid, and some Laravel!</p>
-            <div className="cta">
+            <div className={`${styles.cta}`}>
               <ButtonLink href="/" btnStyle="primary" className="mr-4">VIEW PROJECTS</ButtonLink>
               <ButtonLink href="/" btnStyle="primaryoutline">DOWNLOAD CV</ButtonLink>
             </div>

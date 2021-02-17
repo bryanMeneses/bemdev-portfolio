00d27d68c6502763545210947697767baac2f2fc
Bryan Meneses <bryanmeneses@Bryans-MacBook-Pro.local>
2021-02-17 00:08:51 -0800

    adding next sections in index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   const props = useSpring({
     opacity: 1, 
-    delay: 1250, 
+    delay: 1200, 
     from: {
       opacity: 0
     },
@@ -21,34 +21,52 @@ export default function Home() {
   })
 
   return (
-    <div className={styles.wrapper}>
-      <Head>
+    <>
+     <Head>
         <title>Bryan Meneses | Web Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="container">
-        <div className='flex flex-col items-center justify-between md:flex-row'>
-          <Trail className='mt-4 font-bold text-white md:pr-4 md:mt-0 md:w-7/12 lg:p-0 lg:w-6/12'>
-            <h5 className="uppercase tracking-loose font-bold">React, Next.js, Shopify/Liquid</h5>
-            <h1 className="text-5xl leading-tight my-4">Bryan Meneses - Your Next Front End Developer</h1>
-            <h4 className="leading-normal text-2xl mb-8 font-normal">I love to build experiences on the Web. My aim is to finally let you breathe!</h4>
-            {/* <p className="my-4 font-normal max-w-xl">I am focusing on front end technologies. React, Next.js, jQuery, Shopify/Liquid, and some Laravel!</p> */}
-            <div className={`${styles.cta}`}>
-              <ButtonLink href="/" btnStyle="primary" className="mr-4">VIEW PROJECTS</ButtonLink>
-              <ButtonLink href="/" btnStyle="primaryoutline">DOWNLOAD CV</ButtonLink>
-            </div>
-          </Trail>
-
-          <animated.div style={props} className='w-7/12 md:w-5/12 lg:p-0 lg:w-4/12'>
-            <div className={styles['img-wrap']}>
-              <div className={styles.img}>
-                <Image width="640" height="640" layout="responsive" src="/bryan-portrait.jpeg" />
+      <div className={`${styles.hero}`}>
+        <div className="container max-w-6xl">
+          <div className='flex flex-col items-center justify-between md:flex-row'>
+            <Trail className='mt-4 font-bold text-white md:pr-4 md:mt-0 md:w-7/12 lg:p-0 lg:w-6/12'>
+              <h5 className="uppercase tracking-loose font-bold">React, Next.js, Shopify/Liquid</h5>
+              <h1 className="text-5xl leading-tight my-4">Bryan Meneses - Your Next Front End Developer</h1>
+              <h4 className="leading-normal text-2xl mb-8 font-normal">I love to build experiences on the Web. My aim is to finally let you breathe!</h4>
+              <div className={styles.cta}>
+                <ButtonLink href="/" btnStyle="primary" className="mr-4">VIEW PROJECTS</ButtonLink>
+                <ButtonLink href="/" btnStyle="secondary">DOWNLOAD CV</ButtonLink>
               </div>
-            </div>
-          </animated.div>
+            </Trail>
+
+            <animated.div style={props} className='w-7/12 md:w-5/12 lg:p-0 lg:w-4/12'>
+              <div className={styles['img-wrap']}>
+                <div className={styles.img}>
+                  <Image width="640" height="640" layout="responsive" src="/bryan-portrait.jpeg" />
+                </div>
+              </div>
+            </animated.div>
+          </div>
         </div>
       </div>
-    </div>
+
+      <section className="w-full py-4 bg-white">
+        <div className="container max-w-5xl mx-auto">
+          <h1 className="my-2 text-4xl font-bold tracking-wide text-gray-800 text-center leading-tight">What I <span className="text-my-teal-200">Do</span></h1>
+          <div className="w-full">
+            <div className="h-1 w-64 mx-auto opacity-40 gradient"></div>
+          </div>
+          <div className="flex flex-wrap mt-4">
+            <div className="md:w-2/4">
+              <h2 className="text-3xl text-gray-800 font-bold">E-Commerce</h2>
+              <p className="text-gray-600 text-base my-4 leading-relaxed">Maintained and updated <a href="https://skinresearchlabs.com" target="blank">Skin Research Laboratories</a> for over a year using Shopify. I also used the GSAP libray to for to market a client's new product.</p>
+            </div>
+            <div className="md:w-2/4">
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
